403036a4fcca1cf804639e92f6c17f75875809f7
arjun <m.nawabhag@gmail.com>
2021-11-25 23:40:22 +0530

    eclipse commit

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "C://Users//hpadmin//IdeaProjects//firstMavenIntellij//Features",
+        features = "C://Users//Arjun//IdeaProjects//gittest//Features",
         glue="StepDefinitions",
 dryRun = false,
 plugin = {"pretty","html:test-output"})
